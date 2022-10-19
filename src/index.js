ad88a1a744a9e54c480ee9fde6e96a097e3c787c
Serhii Malychkovych <masevik@gmail.com>
2022-10-19 11:21:09 +0300

    add spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../node_modules/spin.js/spin.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Spinner } from 'spin.js';
 import { refs } from './js/refs';
 import { getImages } from './js/getImages';
 import { createMarkup } from './js/createMarkup';
@@ -8,10 +10,34 @@ import { createMarkup } from './js/createMarkup';
 refs.form.addEventListener('submit', onSearchImage);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
+const opts = {
+  lines: 7, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 0.4, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-shrink', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#6262ef', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
+
+const spinner = new Spinner(opts).spin(refs.spinner);
+
 let page;
 let searchQuery;
 
 function onSearchImage(event) {
+  spinnerStart();
   event.preventDefault();
   clearPage();
   resetPageCounter();
@@ -70,13 +96,17 @@ async function onFetchSuccess() {
       behavior: 'smooth',
     });
   }
+
+  spinnerStop();
 }
 
 function onFetchError(error) {
   console.log('Fetch error:', error.message);
+  spinnerStop();
 }
 
 function onLoadMore() {
+  spinnerStart();
   page += 1;
 
   try {
@@ -105,4 +135,12 @@ function clearPage() {
 
 function resetPageCounter() {
   page = 1;
+}
+
+function spinnerStart() {
+  refs.load.classList.remove('is-hidden');
+}
+
+function spinnerStop() {
+  refs.load.classList.add('is-hidden');
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,6 @@ export const refs = {
   input: document.querySelector('.search-form__input'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
+  spinner: document.querySelector('.js-spinner'),
+  load: document.querySelector('.backdrop'),
 };
